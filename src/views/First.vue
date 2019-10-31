0381e53c2a1bd5aa872f7a7c9d6280887008b8ff
LWangR <17600765487@163.com>
2019-10-31 12:50:04 +0800

    ““首次提交”

---
+++ src/views/First.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+       <div class="bg">你好</div>
+       <div class="chart"></div>
+       <div class="content">
+           <ul>
+               <li></li>
+               <li></li>
+               <li></li>
+           </ul>
+       </div>
+       <div><p></p><p></p></div>
+       <div></div>
+       <div class="zixun">
+           <div class="L"></div>
+           <div></div>
+           <div CLASS="R"></div>
+       </div>
+       <div></div>
+       <div></div>
+
+
+
+    </div>
+</template>
